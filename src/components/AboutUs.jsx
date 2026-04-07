d887de0acc2a2fe102c30aced9dba15a4a77fe23
mihanass <nguyenanhminh05032004@gmail.com>
2026-04-07 21:21:47 +0700

    Add AboutUs component with welcome message

---
+++ src/components/AboutUs.jsx
@@ -0,0 +1,11 @@
+function AboutUs({ goToProducts }) {
+  return (
+    <div>
+      <h1>Paradise Nursery</h1>
+      <p>Welcome to our plant shop 🌿</p>
+      <button onClick={goToProducts}>View Products</button>
+    </div>
+  );
+}
+
+export default AboutUs;
