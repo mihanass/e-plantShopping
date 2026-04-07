5a828ae9e42ae3556d054d1f94698d58c27690db
mihanass <nguyenanhminh05032004@gmail.com>
2026-04-07 21:34:19 +0700

    Show total quantity in cart button
    Display total quantity of items in the cart button.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useSelector } from "react-redux";
 import AboutUs from "./components/AboutUs";
 import ProductList from "./components/ProductList";
 import CartItem from "./components/CartItem";
@@ -6,12 +7,17 @@ import CartItem from "./components/CartItem";
 function App() {
   const [page, setPage] = useState("home");
 
+  // lấy tổng số lượng từ Redux
+  const totalQuantity = useSelector(state => state.cart.totalQuantity);
+
   return (
     <div>
       <nav>
         <button onClick={() => setPage("home")}>Home</button>
         <button onClick={() => setPage("products")}>Products</button>
-        <button onClick={() => setPage("cart")}>Cart</button>
+        <button onClick={() => setPage("cart")}>
+          Cart ({totalQuantity})
+        </button>
       </nav>
 
       {page === "home" && <AboutUs goToProducts={() => setPage("products")} />}
